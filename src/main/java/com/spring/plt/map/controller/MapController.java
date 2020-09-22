780664dd7e8abc7beb965f9c9b890a4e41015296
thdrmsqhd <thdrmsqhd@gmail.com>
2020-09-22 09:13:47 +0900

    create map

---
+++ src/main/java/com/spring/plt/map/controller/MapController.java
@@ -0,0 +1,32 @@
+package com.spring.plt.map.controller;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.spring.plt.map.vo.MapVO;
+
+@Controller
+public class MapController {
+	@Autowired
+	SqlSession sqlSession;
+	
+	@RequestMapping(value="/map/viewMap.do", method = RequestMethod.GET)
+	public ModelAndView viewMap(HttpServletRequest request, HttpServletResponse response) {
+		String viewName = (String)request.getAttribute("viewName");
+		ModelAndView mav = new ModelAndView(viewName);
+		List<MapVO> manuAddrList = sqlSession.selectList("mapper.map.getManuAddrList");
+		mav.addObject("manuAddrList", manuAddrList);
+		System.out.println(mav);
+		return mav;
+	}
+}
